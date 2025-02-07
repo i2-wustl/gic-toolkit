5a0ac7d205629699d704b95ab43464f458928708
indraniel <indraniel@gmail.com>
2025-02-06 18:58:09 -0600

    + first-pass implementation of 'irdb init'
      - creates a duckdb database with the cubes and allids table
        initialized

--- src/gic/tools/subcommands/irdb/init.clj
+++ src/gic/tools/subcommands/irdb/init.clj
@@ -1,9 +1,20 @@
-(ns gic.tools.subcommands.irdb.init)
+(ns gic.tools.subcommands.irdb.init
+  (:require [next.jdbc :as jdbc]
+            [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
+            [gic.tools.utils.db :as u]))
+
+(defn init-irdb! [db-path]
+  (with-open [conn (u/duckdb-connect-rw db-path)]
+    (let [create-cube-sql (slurp (io/resource "sql/irdb/init/create-cubes-table.sql"))
+          create-allids-sql (slurp (io/resource "sql/irdb/init/create-allids-table.sql"))]
+      (run! #(jdbc/execute! conn [%]) (list create-cube-sql create-allids-sql)))))
 
 (defn run [input-opts]
   (let [db-path (get-in input-opts [:opts :dbpath])]
-    (println (format "The duckdb database to initialize is: %s" db-path)))
-  (prn input-opts))
+    (log/info (format "Initializing duckdb database: %s" db-path))
+    (init-irdb! db-path)
+    (log/info "All Done!")))
 
 (comment
   (def test-opts {:dispatch ["init"]
